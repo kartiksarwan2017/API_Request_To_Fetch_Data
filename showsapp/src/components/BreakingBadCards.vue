67222d1860919fc130cda17648193e3d1b339370
rohit8450 <kartiksarwan2017.comp@mmcoe.edu.in>
2024-02-04 13:11:28 +0530

    Fetching Data from External API

--- showsapp/src/components/BreakingBadCards.vue
+++ showsapp/src/components/BreakingBadCards.vue
@@ -46,30 +46,31 @@ import axios from "axios";
     </div>
 </template> -->
 
-
-
-
 <script setup>
 import axios from "axios";
+import { ref, watch } from "vue";
 
-    const options = {
-        method: 'GET',
-        headers: {
-            accept: 'application/json',
-            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4ZjcwOGZkMDlhNGFkZGM3MTczZDA3YzQ2ZGYyMDVlNCIsInN1YiI6IjY0NTkwMDI1NmFhOGUwMDExY2ExNDAzNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.UXbpoC0_kBOKR8Npku7XpKdWH-ELp4k4qTWQUoyRVbk'
-        }
-    };
-    const { data: { results } } = await axios.get(`https://api.themoviedb.org/3/movie/popular`, options);
-    console.log(results);
+    const characters = ref(null);
+    const page = ref(0);
+
+    const response  = await axios.get(`https://pokeapi.co/api/v2/ability/?limit=8`);
+    characters.value = response.data.results;
+
+    watch(page, async () => {
+        const res = await axios.get(`https://pokeapi.co/api/v2/ability/?limit=8&offset=${page.value*8}`);
+        characters.value = res.data.results;
+        console.log(res);
+    });
 
 </script>
 
 <template>
     <div>
         <h1>Breaking Bad Cards</h1>
-        <h1>{{ results }}</h1>
-        <!-- <div v-for="movie in movies" :key="movie.id">
-            <img :src="`https://image.tmdb.org/t/p/w500/${movie.poster_path}`" />
-        </div> -->
+        <h1>{{ characters }}</h1>
+        <div>
+            <button @click="page = page + 1">Next</button>
+            <button @click="page = page - 1">Back</button>
+        </div>
     </div>
 </template>
